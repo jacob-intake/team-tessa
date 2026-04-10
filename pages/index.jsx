4c302a5e4efcccfdd86f07932500b2dd8fbf3d22
jacob-intake <intake@remedyconsumerlaw.com>
2026-04-10 10:25:09 -0700

    Add files via upload

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect, useCallback } from "react";
 import Head from "next/head";
 
-const PHOTO_RECT = { x: 307, y: 286, w: 891, h: 627 };
+const PHOTO_RECT = { x: 307, y: 286, w: 891, h: 915 };
 
 export default function Home() {
   const [propertyImg, setPropertyImg] = useState(null);
